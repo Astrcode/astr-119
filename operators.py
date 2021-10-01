1ee9524fcbeda3e5e4b3cf14c0ea7455b00ca506
Astrcode <91351020+Astrcode@users.noreply.github.com>
2021-09-30 17:58:29 -0700

    Update operators.py

--- operators.py
+++ operators.py
@@ -12,27 +12,28 @@ x=9.191823
 print(x//y) #Floor division
 
 #Assignment operators
-x = 9
-x += 3
+x = 9       #set x = 9
+x += 3      #x = x + 3
 print(x)
-#x = 9
+x = 9       #x = x - 3
 x -= 3
 print(x)
-#x = 9
-x *= 3
+x *= 3      #x = x * 3 
 print(x)
-#x = 9
-x/= 3
-print(x)
-#x = 9
-x **= 3
+x /= 3 		#x = x / 3
+print(x)	
+x **= 3     #x = x ** 3
 print(x)
 
 #Comparison operators
 x = 9
 y = 3
-print("Does x=y? "x==y)   #True if x equals y, False otherwise
-print("Does x!=y? "x!=y)   #True if x doesn't equal y, False otherwise 
+print(x==y)   #True if x equals y, False otherwise
+print(x!=y)   #True if x doesn't equal y, False otherwise 
+print(x>y)    #True if x is greater than y, False otherwise
+print(x<y)    #True if x less than y, False otherwise
+print(x>=y)   #True if x greater than or equal to y, False otherwise
+print(x<=y)   #True if x less than or equal to y, False otherwise
 print("Is x>y? "x>y)    #True if x is greater than y, False otherwise
 print("Is x<y? "x<y)    #True if x less than y, False otherwise
 print("Is x>=y? " x>=y)   #True if x greater than or equal to y, False otherwise
