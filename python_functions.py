8aa1e7b6043675972bedd1df63ad22cbea89ae21
Astrcode <91351020+Astrcode@users.noreply.github.com>
2021-10-01 13:52:17 -0700

    Add files via upload

---
+++ python_functions.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def main():
+	i = 0             	#integers can be delcared with a number
+	n = 10              #another integer  
+	x = 119.0           #floating point nums are declared with a "."
+
+
+	y = np.zeros(n,dtype=float)
+
+	# we can use for loops to declare arrays quickly
+
+	for i in range(n):             #i in range [0,n-1]
+		y[i] = 2.0 * float(i) + 1. #set y = 2i+1 as floats
+
+	# we can also simply iterate through a variable
+	for y_element in y:
+		print(y_element)
+
+#execute the main function
+if __name__ == "__main__":
+	main()
+
+#Works
+
