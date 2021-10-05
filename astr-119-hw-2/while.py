3fe8f926f6a0bf1175d28acdf93b51356ac7609c
Astrcode <91351020+Astrcode@users.noreply.github.com>
2021-10-05 13:21:54 -0700

    Update while.py

--- astr-119-hw-2/while.py
+++ astr-119-hw-2/while.py
@@ -4,4 +4,7 @@ while (i<119):     #while i is less than 119, do the following
 	print(i)       #print the current valye of i
 	i += 10        #add 10 to i
 
-#Works
+'''
+This code starts from i = 0 and adds 10 continuously until i=119 
+(i=120 isn't printed out)
+'''
